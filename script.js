b49aed6b957da85eb739faf0b480ca734026e94b
Barbra Wasiwo <b.wasiwo.bw@gmail.com>
2024-01-01 19:24:24 +0200

    Added a search engine

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+function refeshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+    let apiKey = "e2d2959df01cc6a6d1f4e8cc2a455097"
+    let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    axios.get(apiUrl).then(refeshWeather);
+}
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
@@ -7,3 +20,5 @@ function handleSearchSubmit(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Capetown");
